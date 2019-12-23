fc5ec142580c1e4109c13f4f3d840def9b45541a
srajan1122 <mahabala1122@gmail.com>
2019-12-23 21:06:40 +0530

    created hospital model and two other pages

--- Pages/models.py
+++ Pages/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# x
+
+class Hospital(models.Model):
+    Hospital_id = models.IntegerField(primary_key=True,unique=True,null=False,blank=False)
+    Hospital_name = models.CharField(max_length=100,default='')
+
+    def __str__(self):
+        return self.Hospital_name
+
 class Dept(models.Model):
+    class Meta:
+        unique_together = (('Uid','Hospital_id'),)
+
     Uid=models.IntegerField(primary_key=True,unique=True,null=False,blank=False)
     Type = models.CharField(max_length=20,default='')
     image = models.ImageField(upload_to="department_images/",default="")
+    Hospital_id = models.ForeignKey(Hospital, on_delete=models.CASCADE,default=1)
     
 
     def __str__(self):
         return self.Type 
+
--- Pages/urls.py
+++ Pages/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     re_path(r'^Complete/',views.Complete,name='Complete'),
     re_path(r'^Unknown/',views.Unknown,name='Unknown'),
     re_path(r'^Dept/(?:UID=(?P<UID>\d+)/)?$',views.Department,name='Department'),
+    path("Hospitals/",views.Hospitals,name="Hospitals"),
+    path("Single_hospital/<Hospital_id>",views.Single_hospital,name="Hospital")
     # path("Dept/<UID>",views.Department,name="Department")
     
 ]
