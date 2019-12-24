dd5cc46fd5a4386c911d6bcfa80a5ce97ee76b8a
devdattakhoche <dkhoche70@gmail.com>
2019-12-24 19:57:21 +0530

    Models Correction

--- Pages/models.py
+++ Pages/models.py
@@ -7,13 +7,14 @@ class Hospital(models.Model):
     Hospital_name = models.CharField(max_length=100,default='')
 
     def __str__(self):
-        return self.Hospital_name
+        x=str(self.Hospital_id)
+        return x                                         
 
 class Dept(models.Model):
     class Meta:
         unique_together = (('Uid','Hospital_id'),)
 
-    Uid=models.IntegerField(primary_key=True,unique=True,null=False,blank=False)
+    Uid = models.IntegerField()
     Type = models.CharField(max_length=20,default='')
     image = models.ImageField(upload_to="department_images/",default="")
     Hospital_id = models.ForeignKey(Hospital, on_delete=models.CASCADE,default='null')
