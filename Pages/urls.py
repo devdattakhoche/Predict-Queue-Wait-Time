c63584272f4bd53c92b05164d96080d7c5649b65
srajan1122 <mahabala1122@gmail.com>
2019-12-29 17:11:51 +0530

    single shows department of a particular hospital

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,17 +1,14 @@
 from . import views
 from django.urls import re_path ,path
 
-
 urlpatterns = [
     re_path(r'^Documentation/',views.Documentation,name='Documentation'),
-    re_path(r'^Single/',views.Single,name='Single'),
-    re_path(r'^Complete/',views.Complete,name='Complete'),
-    re_path(r'^Unknown/',views.Unknown,name='Unknown'),
-    path('Dept/Hid=<int:Hid>/Uid=<int:UID>',views.Department,name='Department'),
     re_path(r'^Hospitals/',views.Hospitals,name="Hospitals"),
-    re_path(r'^Dashboard/(?:Huid-(?P<Hospital_id>\d+)/)?$',views.Dashboard,name="Dashboard"),
-    # path("Single_hospital/<Hospital_id>",views.Single_hospital,name="Hospital")
-    # path("Dept/<UID>",views.Department,name="Department")
-    
+    path("Hospital/<Hospital_id>/Single",views.Single,name="Single"),
+    path("Hospital/<Hospital_id>/Dashboard",views.Dashboard,name="Dashboard"),
+    path("Hospital/<Hospital_id>/Complete",views.Complete,name="Complete"),
+    path("Hospital/<Hospital_id>/Unknown",views.Unknown,name="Unknown"),
+    path("Hospital/<Hospital_id>/Dept/<UID>",views.Department,name="Department")
+  
 ]
 
--- Pages/views.py
+++ Pages/views.py
@@ -4,38 +4,70 @@ from .models import Dept,Hospital
 
 def Documentation(request):
     return render(request,'Pages/Documentation.html')
-
-def Single(request):
-    x=Dept.objects.all()
-    y = Dept.objects.get(Uid=1)
-    print(y.image)  
-    params={'Pro':x}
-    if request.method== "POST" :
-        No=request.POST.get('Dept_id') 
-        return redirect('/Dept/UID='+No)
-    return render(request,'Pages/Single.html',params)
-
-def Complete(request):
-    return render(request,'Pages/Complete.html')
-
-def Unknown(request):
-    return render(request,'Pages/Unknown.html')
     
-def Department(request,Hid,UID):
-    x=Dept.objects.get(Uid=UID)
-    y=Hospital.objects.get(Hospital_id=Hid)
-    params={'Pro':x,'Hos':y}
-    return render(request,'Pages/Department.html',params)
-def Dashboard(request,Hospital_id):
-    x=Hospital.objects.get(Hospital_id=Hospital_id)
-    params={'Pro':x}
-    return render(request,'Pages/Dashboard.html',params)
-
 def Hospitals(request):
-    x = Hospital.objects.all()
+    x = Hospital.objects.all()  
     params={'Pro':x}
     if request.method== "POST" :
         No=request.POST.get('Hospital_id')
-        return redirect('/Dashboard/Huid-'+No)
+        return redirect('http://localhost:8000/Hospital/'+No+'/Dashboard')
     return render(request,'Pages/Hospitals.html',params)
 
+def Dashboard(request,Hospital_id):
+    x=Hospital.objects.get(Hospital_id=Hospital_id)
+    params={'HosId_id':x}
+    return render(  request,'Pages/Dashboard.html',params)
+
+def Single(request,Hospital_id):
+    a=Hospital.objects.get(Hospital_id=Hospital_id)
+    x=Dept.objects.all()
+
+    DepartmentList = []
+
+    for i in x:
+        DepartmentDict = {}
+        DepartmentDict['Uid'] = str(i.Uid) 
+        DepartmentDict['Type'] = str(i.Type)
+        DepartmentDict['Image'] = i.image
+        DepartmentDict['Hospital_id'] = str(i.Hospital_id)
+        DepartmentList.append(DepartmentDict)
+
+    params={'Pro':DepartmentList,'HosId':Hospital_id,'HosId_id':a}
+    if request.method== "POST" :
+        No=request.POST.get('Dept_id') 
+        return redirect('http://localhost:8000/Hospital/'+Hospital_id+'/Dept/' + No)
+    return render(request,'Pages/Single.html',params)
+
+def Complete(request,Hospital_id):
+    x=Hospital.objects.get(Hospital_id=Hospital_id)
+    params={'HosId_id':x}
+    return render(request,'Pages/Complete.html',params)
+
+def Unknown(request,Hospital_id):
+    x=Hospital.objects.get(Hospital_id=Hospital_id)
+    params={'HosId_id':x}
+    return render(request,'Pages/Unknown.html',params)
+    
+def Department(request,Hospital_id,UID):
+    a = Hospital.objects.get(Hospital_id=Hospital_id)
+    x = Dept.objects.all()
+    y = Hospital.objects.all()
+    
+    DepartmentList = []
+    HospitalList = []
+
+    for i in x:
+        DepartmentDict = {}
+        DepartmentDict['Uid'] = str(i.Uid) 
+        DepartmentDict['Type'] = str(i.Type)
+        DepartmentDict['Hospital_id'] = str(i.Hospital_id)
+        DepartmentList.append(DepartmentDict)
+
+    for i in y:
+        HospitalDict = {}
+        HospitalDict['Hospital_name'] = str(i.Hospital_name) 
+        HospitalDict['Hospital_id'] = str(i.Hospital_id)
+        HospitalList.append(HospitalDict)
+
+    params={'Pro':DepartmentList,'Hos':HospitalList,'ProId':UID,'HosId':Hospital_id,'HosId_id':a}
+    return render(request,'Pages/Department.html',params)
