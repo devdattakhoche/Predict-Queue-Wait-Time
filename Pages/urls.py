bc041524b2017b15ff3365eda0cd6907e44864e2
devdattakhoche <dkhoche70@gmail.com>
2020-01-05 17:21:18 +0530

    Machine Leaning Model

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,16 +1,17 @@
 from . import views
-from django.urls import re_path ,path
+from django.urls import re_path, path
 
 urlpatterns = [
-    re_path(r'^Documentation/',views.Documentation,name='Documentation'),
-    re_path(r'^Hospitals/',views.Hospitals,name="Hospitals"),
-    path("Hospital/<Hospital_id>/Single",views.Single,name="Single"),
-    path("Hospital/<Hospital_id>/Dashboard",views.Dashboard,name="Dashboard"),
-    path("Hospital/<Hospital_id>/Complete",views.Complete,name="Complete"),
-    path("Hospital/<Hospital_id>/Unknown",views.Unknown,name="Unknown"),
-    path("Hospital/<Hospital_id>/Dept/<UID>",views.Department,name="Department"),
-    path("Predict",views.Predict,name="Predict"),
-    path("Train",views.Train,name="Train"),
-  
-]
+    re_path(r'^Documentation/', views.Documentation, name='Documentation'),
+    re_path(r'^Hospitals/', views.Hospitals, name="Hospitals"),
+    path("Hospital/<Hospital_id>/Single", views.Single, name="Single"),
+    path("Hospital/<Hospital_id>/Dashboard",
+         views.Dashboard, name="Dashboard"),
+    path("Hospital/<Hospital_id>/Complete", views.Complete, name="Complete"),
+    path("Hospital/<Hospital_id>/Unknown", views.Unknown, name="Unknown"),
+    path("Hospital/<Hospital_id>/Dept/<UID>",
+         views.Department, name="Department"),
+    path("Predict", views.Predict, name="Predict"),
+    path("Train", views.Train, name="Train"),
 
+]
--- Pages/views.py
+++ Pages/views.py
@@ -1,134 +1,202 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from .models import Dept,Hospital
+from .models import Dept, Hospital
 from .filter import HospitalFilter
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
 from sklearn import metrics
 import pandas as pd
 import pickle
+import calendar
 import os
 import joblib
 from django.conf import settings
 
 
-
 def Documentation(request):
-    return render(request,'Pages/Documentation.html')
-    
+    print("Printed in ViewsDocumentation")
+    return render(request, 'Pages/Documentation.html')
+
+
 def Hospitals(request):
-    x = Hospital.objects.all()      
-    Hospital_filter = HospitalFilter(request.GET,queryset=x)
+    print("Printed in Views.Hospitals")
+    x = Hospital.objects.all()
+    Hospital_filter = HospitalFilter(request.GET, queryset=x)
     # HospitalFilter(request.GET).data['Hospital_name'].upper()
-    params={'Pro':x,'filter':Hospital_filter}
-    if request.method== "POST" :
-        No=request.POST.get('Hospital_id')
+    params = {'Pro': x, 'filter': Hospital_filter}
+    if request.method == "POST":
+        No = request.POST.get('Hospital_id')
         return redirect('http://localhost:8000/Hospital/'+No+'/Dashboard')
-    return render(request,'Pages/Hospitals.html',params)
+    return render(request, 'Pages/Hospitals.html', params)
 
-def Dashboard(request,Hospital_id):
-    x=Hospital.objects.get(Hospital_id=Hospital_id)
-    params={'HosId_id':x}
-    return render(  request,'Pages/Dashboard.html',params)
 
-def Single(request,Hospital_id):
-    a=Hospital.objects.get(Hospital_id=Hospital_id)
-    x=Dept.objects.all()
+def Dashboard(request, Hospital_id):
+    print("Printed in Views.Dashboard")
+    x = Hospital.objects.get(Hospital_id=Hospital_id)
+    params = {'HosId_id': x}
+    return render(request, 'Pages/Dashboard.html', params)
+
+
+def Single(request, Hospital_id):
+    print("Printed in Views.Single")
+    a = Hospital.objects.get(Hospital_id=Hospital_id)
+    x = Dept.objects.all()
 
     DepartmentList = []
 
     for i in x:
         DepartmentDict = {}
-        DepartmentDict['Uid'] = str(i.Uid) 
+        DepartmentDict['Uid'] = str(i.Uid)
         DepartmentDict['Type'] = str(i.Type)
         DepartmentDict['Image'] = i.image
         DepartmentDict['Hospital_id'] = str(i.Hospital_id)
         DepartmentList.append(DepartmentDict)
 
-    params={'Pro':DepartmentList,'HosId':Hospital_id,'HosId_id':a}
-    if request.method== "POST" :
-        No=request.POST.get('Dept_id') 
+    params = {'Pro': DepartmentList, 'HosId': Hospital_id, 'HosId_id': a}
+    if request.method == "POST":
+        No = request.POST.get('Dept_id')
         return redirect('/Hospital/'+Hospital_id+'/Dept/' + No)
-    return render(request,'Pages/Single.html',params)
+    return render(request, 'Pages/Single.html', params)
 
-def Complete(request,Hospital_id):
-    x=Hospital.objects.get(Hospital_id=Hospital_id)
-    params={'HosId_id':x}
-    return render(request,'Pages/Complete.html',params)
 
-def Unknown(request,Hospital_id):
-    x=Hospital.objects.get(Hospital_id=Hospital_id)
-    params={'HosId_id':x}
-    return render(request,'Pages/Unknown.html',params)
-    
-def Department(request,Hospital_id,UID):
+def Complete(request, Hospital_id):
+    print("Printed in Views.Complete")
+    x = Hospital.objects.get(Hospital_id=Hospital_id)
+    params = {'HosId_id': x}
+    return render(request, 'Pages/Complete.html', params)
+
+
+def Unknown(request, Hospital_id):
+    print("Printed in Views.Unknown")
+    x = Hospital.objects.get(Hospital_id=Hospital_id)
+    params = {'HosId_id': x}
+    return render(request, 'Pages/Unknown.html', params)
+
+
+def Department(request, Hospital_id, UID):
+    print("Printed in Views.Department")
     a = Hospital.objects.get(Hospital_id=Hospital_id)
     x = Dept.objects.all()
     y = Hospital.objects.all()
-    
+
     DepartmentList = []
     HospitalList = []
 
     for i in x:
         DepartmentDict = {}
-        DepartmentDict['Uid'] = str(i.Uid) 
+        DepartmentDict['Uid'] = str(i.Uid)
         DepartmentDict['Type'] = str(i.Type)
         DepartmentDict['Hospital_id'] = str(i.Hospital_id)
         DepartmentList.append(DepartmentDict)
 
     for i in y:
         HospitalDict = {}
-        HospitalDict['Hospital_name'] = str(i.Hospital_name) 
+        HospitalDict['Hospital_name'] = str(i.Hospital_name)
         HospitalDict['Hospital_id'] = str(i.Hospital_id)
         HospitalList.append(HospitalDict)
 
-    params={'Pro':DepartmentList,'Hos':HospitalList,'ProId':UID,'HosId':Hospital_id,'HosId_id':a}
-    return render(request,'Pages/Department.html',params)
-    
+    params = {'Pro': DepartmentList, 'Hos': HospitalList,
+              'ProId': UID, 'HosId': Hospital_id, 'HosId_id': a}
+    return render(request, 'Pages/Department.html', params)
+
+
 def Train(request):
-    iris = datasets.load_iris()
-    data=pd.DataFrame({
-        'sepal length':iris.data[:,0],
-        'sepal width':iris.data[:,1],
-        'petal length':iris.data[:,2],
-        'petal width':iris.data[:,3],
-        'species':iris.target
-    })
-    X=data[['sepal length', 'sepal width', 'petal length', 'petal width']]  # Features
-    y=data['species']  # Labels
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    clf=RandomForestClassifier(n_estimators=100)
-    clf.fit(X_train,y_train)
-    y_pred=clf.predict(X_test)
-    print("Accuracy:",metrics.accuracy_score(y_test, y_pred)*100)
-
-    path = os.path.join(settings.MODEL_ROOT, 'clf')
+    print("Printed in Views.Train")
+    column_name = ['Queue_number', 'Shift', 'date', 'Created_queue_hours',
+                   'Waiting_duration', 'Number_of_wating_queue', 'Arrival_rate', 'Service_rate']
+    data = pd.read_csv('http://bit.ly/deepblue_data',
+                       header=0, names=column_name)
+    dt = data['date'].str.split('/')
+    dt2 = []
+    for i in dt:
+        i[0], i[1] = i[1], i[0]
+        string = '/'
+        i = string.join(i)
+        dt2.append(i)
+    data['date'] = pd.Series(dt2)
+    data['date'] = pd.to_datetime(data['date'])
+    data['weekdays'] = data['date'].dt.weekday
+    data.drop('date', axis=1, inplace=True)
+    y = data['Waiting_duration']
+    X = data.drop('Waiting_duration', axis=1)
+    # y = np.array(y)
+    # X = np.array(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=42)
+    rf = RandomForestRegressor(n_estimators=1000, random_state=42)
+    rf.fit(X_train, y_train)
+    prediction = rf.predict(X_test)
+    error = abs(prediction - y_test)
+    mape = 100*(error/y_test)
+    accuracy = 100 - np.mean(mape)
+    print('Accuracy:', round(accuracy, 2), '%.')
+    path = os.path.join(settings.MODEL_ROOT, 'rf')
     with open(path, 'wb') as file:
-        joblib.dump(clf, 'clf.pkl') 
-        # pickle.dump(clf, file)
-    
+        joblib.dump(rf, 'rf.pkl')
+        joblib.dump(data, 'data.pkl')
+    prediction_parameter = ([[101,   2,  14,  40,   4,   2,   6]])
+    rf.predict(prediction_parameter)
 
-    return render(request,'Pages/sucess.html')
+    return render(request, 'Pages/sucess.html')
 
 
 def Predict(request):
-    z=None
-    params = { 'pro': z }
+
+    z = None
+    print("Printed in Views.Predict")
+    params = {'pro': z}
     if request.method == "POST":
-        First=request.POST.get('First')
-        Second=request.POST.get('Second')
-        Third=request.POST.get('Third') 
-        Fourth=request.POST.get('Fourth')
-
-        path = os.path.join(settings.MODEL_ROOT, 'clf')
+        Queue_number = request.POST.get('Queue_number')
+        Queue_number = int(Queue_number)
+        Date = request.POST.get('Date')
+        year, month, day = (int(i) for i in Date.split('-'))
+        dayNumber = calendar.weekday(year, month, day)
+        days = ["Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday", "Sunday"]
+        print(dayNumber, days[dayNumber])
+        Time = request.POST.get('Time')
+        Hour, Mins = (int(i) for i in Time.split(':'))
+        if Mins > 30:
+            Hour = Hour + 1
+        Hour = int(Hour)
+        print(Hour)
+        print(Queue_number, type(Queue_number),
+              Date, type(Date), Time, type(Time))
+        path = os.path.join(settings.MODEL_ROOT, 'rf')
         with open(path, 'rb') as file:
-            model = joblib.load('clf.pkl')  
-            # model = pickle.load(file)
-             
+            model = joblib.load('rf.pkl')
+            data = joblib.load('data.pkl')
+        print(type(data))
+        print(data.head())
+        shift = None
+        if Hour in [8, 9, 10, 11, 12]:
+            shift = 1
+        elif Hour in [13, 14, 15, 16, 17, 18]:
+            shift = 2
+        else:
+            shift = 3
 
-        z=model.predict([[First,Second,Third,Fourth]])
-        params = {'pro':z}
+        arrival_rate = data[(data['Created_queue_hours'] == Hour) & (
+            data['weekdays'] == dayNumber)]['Arrival_rate'].max()
+        service_rate = data[(data['Created_queue_hours'] == Hour) & (
+            data['weekdays'] == dayNumber)]['Service_rate'].min()
+        waiting_queue = data[(data['Created_queue_hours'] == Hour) & (
+            data['weekdays'] == dayNumber)]['Number_of_wating_queue'].max()
+        prediction_list = [Queue_number, shift, Hour,
+                           waiting_queue, arrival_rate, service_rate, dayNumber]
+        prediction_parameter = ([prediction_list])
+        z = model.predict(prediction_parameter)
+        print(z, type(z))
+        z = str(z)
+        z = z[1:-1]
+        print(z, type(z))
+        z = float(z)
+        print("{0:.2f}".format(round(z, 2)))
+        z = "{0:.2f}".format(round(z, 2))
 
-    return render(request,'Pages/Machine_learning.html',params)
+        params = {'pro': z}
 
+    return render(request, 'Pages/Machine_learning.html', params)
