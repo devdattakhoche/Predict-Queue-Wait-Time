8c77a7cc4033e994288b2882997dbe3899bad11b
devdattakhoche <dkhoche70@gmail.com>
2020-01-06 15:33:52 +0530

    Indentation

--- Pages/models.py
+++ Pages/models.py
@@ -2,25 +2,27 @@ from django.db import models
 
 # x
 
+
 class Hospital(models.Model):
-    Hospital_id = models.IntegerField(primary_key=True,unique=True,null=False,blank=False)
-    Hospital_name = models.CharField(max_length=100,default='')
-    Image = models.ImageField(upload_to="Hospital_images/",default="")
+    Hospital_id = models.IntegerField(
+        primary_key=True, unique=True, null=False, blank=False)
+    Hospital_name = models.CharField(max_length=100, default='')
+    Image = models.ImageField(upload_to="Hospital_images/", default="")
 
     def __str__(self):
-        x=str(self.Hospital_id)
-        return x                                         
+        x = str(self.Hospital_id)
+        return x
+
 
 class Dept(models.Model):
     class Meta:
-        unique_together = (('Uid','Hospital_id'),)
+        unique_together = (('Uid', 'Hospital_id'),)
 
     Uid = models.IntegerField()
-    Type = models.CharField(max_length=20,default='')
-    image = models.ImageField(upload_to="department_images/",default="")
-    Hospital_id = models.ForeignKey(Hospital, on_delete=models.CASCADE,default='null')
-    
+    Type = models.CharField(max_length=20, default='')
+    image = models.ImageField(upload_to="department_images/", default="")
+    Hospital_id = models.ForeignKey(
+        Hospital, on_delete=models.CASCADE, default='null')
 
     def __str__(self):
-        return self.Type 
-
+        return self.Type
