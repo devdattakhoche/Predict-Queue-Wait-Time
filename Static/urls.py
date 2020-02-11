acdbb2b279c5e23ef65d27f153424bbc8c63d049
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-02-12 00:47:48 +0530

    color scheme added

--- Static/urls.py
+++ Static/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Pages.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
         urlpatterns += staticfiles_urlpatterns()
         urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
