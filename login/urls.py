2c9fb324cdad3b5ab7be5780cec40c4a1d816d1a
srajan1122 <mahabala1122@gmail.com>
2020-02-13 23:46:35 +0530

    login and logout

--- login/urls.py
+++ login/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('login/',views.login,name='login'),
     path('registration/',views.registration,name='registration'),
+    path('index/',views.index,name='index'),
+    path('logout/',views.user_logout,name='logout'),
 ]
 
--- login/views.py
+++ login/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    return render(request,'../templates/loginFolder/index.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('index'))
 
 def login(request):
+    if request.method == 'POST':
+        name = request.POST.get('Hospital_name')
+        password = request.POST.get('password')
+        user = authenticate(username=name,password=password)
+        if user:
+            if user.is_active:
+                auth_login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your account was inactive.")
+        else:
+            print("Someone tried to login and failed.")
+            
     return render(request,'../templates/loginFolder/login.html')
 
 def registration(request):
