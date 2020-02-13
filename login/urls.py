a777ae29398b16d8523e886c8ecca6fd6a050526
srajan1122 <mahabala1122@gmail.com>
2020-02-14 03:26:33 +0530

    overall logic

--- login/urls.py
+++ login/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('registration/',views.registration,name='registration'),
     path('index/',views.index,name='index'),
     path('logout/',views.user_logout,name='logout'),
+    path('delele/',views.department_delete,name='delete'),
+    path('update/',views.department_update,name='update'),
+    path('update-form/<slug:slug>',views.update_form,name='update_form')
 ]
-
--- login/views.py
+++ login/views.py
@@ -4,8 +4,15 @@ from django.contrib.auth import authenticate, login as auth_login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from Pages.models import Hospital,Dept
+from django.core.files.storage import FileSystemStorage
 
 def index(request):
+    Hospital_id = Hospital.objects.get(Hospital_name=request.user.username)
+    if request.method=='POST':
+        for i in range(1,int(request.POST['srajan1'])):
+            Department_name = request.POST['Department[{}][name]'.format(i)]
+            Dept_instance = Dept.objects.create(Type=Department_name,Hospital_id=Hospital_id)
     return render(request,'../templates/loginFolder/index.html')
 
 @login_required
@@ -33,7 +40,38 @@ def registration(request):
     if request.method=='POST':
         name = request.POST.get('Hospital_name')
         password = request.POST.get('password')
+        image = request.POST.get('image123')
         user = User.objects.create_user(username=name,password=password)
-        print(name,password)
-        return redirect(reverse('login'))
+        print(name,password,image)
+        hospital_instance = Hospital.objects.create(Hospital_name=name)
     return render(request,'../templates/loginFolder/registration.html')
+
+@login_required
+def department_delete(request):
+    Hospital_id = Hospital.objects.get(Hospital_name=request.user.username)
+    x = Dept.objects.filter(Hospital_id=Hospital_id)
+    params = {'dept':x,'hospital_id':Hospital_id}
+    if request.method == 'POST':
+        print(request.POST['hidden'])
+        y = Dept.objects.get(Uid=request.POST['hidden'])
+        y.delete()
+        return render(request,'../templates/loginFolder/delete.html',params)
+
+    return render(request,'../templates/loginFolder/delete.html',params)
+
+@login_required
+def department_update(request):
+    if request.method == 'POST':
+        x = Dept.objects.get(Uid=request.POST['hidden'])
+        x.Type = request.POST['name']
+        x.save()
+
+    Hospital_id = Hospital.objects.get(Hospital_name=request.user.username)
+    x = Dept.objects.filter(Hospital_id=Hospital_id)
+    params = {'dept':x,'hospital_id':Hospital_id}
+    
+    return render(request,'../templates/loginFolder/update.html',params)
+
+@login_required
+def update_form(request,slug):
+    return render(request,'../templates/loginFolder/update_form.html',{'slug':slug})
